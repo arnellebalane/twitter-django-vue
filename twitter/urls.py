e3f54d4a3713d19d564486c542b18bd1e6447d21
Arnelle Balane <arnellebalane@gmail.com>
2018-07-23 11:37:12 +0800

    feat(urls): add DRF auth urls

--- twitter/urls.py
+++ twitter/urls.py
@@ -29,5 +29,6 @@ router.register(r'tweets', views.TweetViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
