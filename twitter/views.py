89beee4b1600ebfa114ccb0b75e33f91b76defa0
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 00:41:40 +0800

    feat(views): add custom RegisterView APIView for registering users

--- twitter/views.py
+++ twitter/views.py
@@ -1,9 +1,30 @@
 from django.contrib.auth.models import User
 
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Profile, Tweet
 from .serializers import UserSerializer, ProfileSerializer, TweetSerializer
+
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        user_serializer = UserSerializer(data=request.data)
+        profile_serializer = ProfileSerializer(data=request.data)
+
+        if user_serializer.is_valid() and profile_serializer.is_valid():
+            user = User.objects.create_user(
+                user_serializer.validated_data.get('username'),
+                user_serializer.validated_data.get('email'),
+                request.data.get('password'))
+            profile = profile_serializer.save(user=user)
+
+            return Response(profile_serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(profile_serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserViewSet(viewsets.ModelViewSet):
