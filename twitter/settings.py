d12e015a23b08aa338c903109307d8fb64bea6db
Arnelle Balane <arnellebalane@gmail.com>
2018-07-23 11:35:39 +0800

    feat(settings): add REST_FRAMEWORK settings with permission classes

--- twitter/settings.py
+++ twitter/settings.py
@@ -122,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# REST Framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+}
