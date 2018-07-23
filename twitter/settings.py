1f360c42738c8fd72e02b99ecec0ebc5b0542643
Arnelle Balane <arnellebalane@gmail.com>
2018-07-23 23:22:37 +0800

    feat(settings): use JWT authentication in REST_FRAMEWORK settings

--- twitter/settings.py
+++ twitter/settings.py
@@ -130,4 +130,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
 }
