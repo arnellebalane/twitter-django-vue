771a982a0979eebf87acbee7066f7a26f788bbd9
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 20:46:37 +0800

    feat(pages): define RegisterPage component

---
+++ twitter/static/twitter/pages/RegisterPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="register-page auth-page">
+        <RegisterForm />
+    </div>
+</template>
+
+<script>
+    import RegisterForm from 'source/modules/auth/components/RegisterForm.vue';
+
+    export default {
+        name: 'RegisterPage',
+
+        components: {
+            RegisterForm
+        }
+    };
+</script>
+
+<style scoped src="source/stylesheets/shared/auth-pages.css"></style>
