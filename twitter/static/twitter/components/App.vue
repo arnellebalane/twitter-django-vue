8299db4cf5dfe661dae8519e51f10ce0e9ebbf4e
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 22:27:45 +0800

    feat(components): render AppNavbar inside App component

--- twitter/static/twitter/components/App.vue
+++ twitter/static/twitter/components/App.vue
@@ -1,11 +1,18 @@
 <template>
     <div id="app" class="app">
+        <AppNavbar />
         <router-view />
     </div>
 </template>
 
 <script>
+    import AppNavbar from './AppNavbar.vue';
+
     export default {
-        name: 'App'
+        name: 'App',
+
+        components:{
+            AppNavbar
+        }
     };
 </script>
