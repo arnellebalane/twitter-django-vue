b44981e18ec63c51c86620658591d06927770927
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 20:57:09 +0800

    feat(store): add auth store into main store modules

--- twitter/static/twitter/store/index.js
+++ twitter/static/twitter/store/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import auth from 'source/modules/auth/store';
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({});
+const store = new Vuex.Store({
+    modules: {
+        auth
+    }
+});
 
 export default store;
