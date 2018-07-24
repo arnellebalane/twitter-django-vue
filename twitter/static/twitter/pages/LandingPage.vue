64d791805b68821b99b065bef64df84f0258d0b0
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 22:44:17 +0800

    feat(pages): define LandingPage component

---
+++ twitter/static/twitter/pages/LandingPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <FeedPage v-if="isLoggedIn" />
+    <LoginPage v-else />
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+
+    export default {
+        name: 'LandingPage',
+
+        components: {
+            FeedPage: () => import('./FeedPage.vue'),
+            LoginPage: () => import('./LoginPage.vue')
+        },
+
+        computed: mapGetters('auth', ['isLoggedIn'])
+    };
+</script>
