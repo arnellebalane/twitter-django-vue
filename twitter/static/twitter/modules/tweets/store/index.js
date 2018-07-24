b0f0e60903dfe5ad99753851d621ec93854233b4
Arnelle Balane <arnellebalane@gmail.com>
2018-07-25 07:53:09 +0800

    feat(store): add createTweet and tweets state in tweets module store

--- twitter/static/twitter/modules/tweets/store/index.js
+++ twitter/static/twitter/modules/tweets/store/index.js
@@ -1,10 +1,23 @@
-const state = {};
+import axios from 'source/lib/axios';
+
+const state = {
+    tweets: []
+};
 
 const getters = {};
 
-const mutations = {};
+const mutations = {
+    addTweet(state, tweet) {
+        state.tweets = [tweet, ...state.tweets];
+    }
+};
 
-const actions = {};
+const actions = {
+    async createTweet({commit}, data) {
+        const tweet = await axios.post('/api/tweets/', data);
+        commit('addTweet', tweet);
+    }
+};
 
 export default {
     namespaced: true,
