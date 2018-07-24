a21ca69fdfdbef3680e2b913a6871ee82e682556
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 20:50:42 +0800

    feat(vue): inject Vuex store into Vue instance

--- twitter/static/twitter/index.js
+++ twitter/static/twitter/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import router from './router';
+import store from './store';
 import App from './components/App.vue';
 import './stylesheets/index.css';
 
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 });
