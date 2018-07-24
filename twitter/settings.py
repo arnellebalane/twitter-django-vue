3ec164bca5a7366ece86afb09ee110b9a8970edd
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 11:06:00 +0800

    feat(settings): change STATIC_URL to point to nginx

--- twitter/settings.py
+++ twitter/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://localhost:8003/'
 
 STATIC_ROOT = 'static-root'
 
