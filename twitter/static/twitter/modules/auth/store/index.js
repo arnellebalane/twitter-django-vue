ade2e38050a6d4e62a953e38a8d5c50be39feec2
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 21:10:02 +0800

    feat(store): add performLogin action to auth module store

--- twitter/static/twitter/modules/auth/store/index.js
+++ twitter/static/twitter/modules/auth/store/index.js
@@ -1,10 +1,27 @@
-const state = {};
+import axios from 'source/lib/axios';
+
+const state = {
+    token: null
+};
 
 const getters = {};
 
-const mutations = {};
+const mutations = {
+    setToken(state, token) {
+        state.token = token;
+    },
 
-const actions = {};
+    clearToken(state) {
+        state.token = null;
+    }
+};
+
+const actions = {
+    async performLogin({commit}, data) {
+        const {token} = await axios.post('/api-token-auth/', data);
+        commit('setToken', token);
+    }
+};
 
 export default {
     namespaced: true,
