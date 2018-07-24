1c0e897a87a59716a16ddfde62acdc180d2e2bc0
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 22:33:55 +0800

    feat(pages): define FeedPage component

---
+++ twitter/static/twitter/pages/FeedPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="feed-page">
+        <div class="wrapper">
+            <TweetForm />
+        </div>
+    </div>
+</template>
+
+<script>
+    import TweetForm from 'source/modules/tweets/components/TweetForm.vue';
+
+    export default {
+        name: 'FeedPage',
+
+        components: {
+            TweetForm
+        }
+    };
+</script>
+
+<style scoped>
+    .wrapper {
+        width: 90vw;
+        max-width: 60rem;
+        padding: 1.2rem 0;
+        margin: 0 auto;
+    }
+</style>
