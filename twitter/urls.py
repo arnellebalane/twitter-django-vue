7201b22a973782989893f2e5aaadc804f03a9818
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 22:17:37 +0800

    feat(urls): add url route to CurrentUserView

--- twitter/urls.py
+++ twitter/urls.py
@@ -31,6 +31,7 @@ router.register(r'tweets', views.TweetViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/me/', views.CurrentUserView.as_view()),
     path('api-auth/', include('rest_framework.urls')),
     path('api-auth/register/', views.RegisterView.as_view()),
     path('api-token-auth/', obtain_jwt_token),
