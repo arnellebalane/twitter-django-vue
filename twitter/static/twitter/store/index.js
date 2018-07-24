0aab788fe984e8a189c1a47724aae38d79b207d6
Arnelle Balane <arnellebalane@gmail.com>
2018-07-25 07:52:06 +0800

    feat(store): load twitter module store in main store object

--- twitter/static/twitter/store/index.js
+++ twitter/static/twitter/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import auth from 'source/modules/auth/store';
+import tweets from 'source/modules/tweets/store';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     modules: {
-        auth
+        auth,
+        tweets
     }
 });
 
