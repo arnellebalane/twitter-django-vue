7bd787c2301ed8f056bd84a093a7c8d1049470f5
Arnelle Balane <arnellebalane@gmail.com>
2018-07-24 11:18:21 +0800

    build(webpack): define loader rules in webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -10,5 +10,24 @@ export default {
         publicPath: 'http://localhost:8003/'
     },
 
-    mode: process.env.NODE_ENV || 'development'
+    mode: process.env.NODE_ENV || 'development',
+
+    module: {
+        rules: [ {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        }, {
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'babel-loader']
+        }, {
+            test: /\.(png|jpe?g|gif|svg)$/,
+            loader: 'url-loader',
+            options: {
+                limit: 1024
+            }
+        } ]
+    }
 };
