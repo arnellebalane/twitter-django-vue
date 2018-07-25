989d6fde9471a24f4fb33c1b36695ca7a39595dd
Arnelle Balane <arnellebalane@gmail.com>
2018-07-25 12:12:25 +0800

    feat(components): make AppNavbar sticky on top of the page

--- twitter/static/twitter/components/App.vue
+++ twitter/static/twitter/components/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app" class="app">
         <AppNavbar />
-        <router-view />
+        <router-view class="app-page" />
     </div>
 </template>
 
@@ -16,3 +16,16 @@
         }
     };
 </script>
+
+<style scoped>
+    .app-navbar {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+
+    .app-page {
+        padding-top: 5rem;
+    }
+</style>
