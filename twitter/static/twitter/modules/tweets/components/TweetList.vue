0ab0a47d7f76bb46b1bd420974afc40d88c1d61d
Arnelle Balane <arnellebalane@gmail.com>
2018-07-25 08:04:26 +0800

    feat(components): add skeleton TweetList component

---
+++ twitter/static/twitter/modules/tweets/components/TweetList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="tweet-list">
+        <h1>Tweet List</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TweetList',
+
+        props: {
+            tweets: {
+                type: Array,
+                required: true
+            }
+        }
+    };
+</script>
